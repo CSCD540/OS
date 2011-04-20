172623ca5f82430d960ddf7b63bad0decfb27159
Brian Zier <zier.brian@gmail.com>
2011-04-20 15:24:42 -0700

    Added my page table testing file (pt.c) This is where I am starting development on the page table function

---
+++ dev/pt.c
@@ -0,0 +1,20 @@
+
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define PAGESIZE 4 //size of each page
+#define NUMPAGES 2 //Number of pages in page table
+
+int page_table(int vpn);
+
+main(int argc, char *argv[])
+{
+    page_table();
+}
+
+int page_table(int vpn)
+{
+    
+}
+
